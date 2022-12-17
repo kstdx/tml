b16b61f0011f320d3eb7b984b2a13b9eaf25320d
kstdx <kstdx@users.noreply.github.com>
2022-12-17 18:04:10 +0900

    add an example "webcomponent.html"

--- src/tag.js
+++ src/tag.js
@@ -1,15 +1,3 @@
-const buildProps = (element, props) => {
-    for (name in props) {
-        if (name[0] === ':') {
-            element.addEventListener(name.slice(1), props[name])
-        } else {
-            element.setAttribute(name, props[name])
-        }
-    }
-
-    return element
-}
-
 export const tag = (tag, props, ...children) => {
     if (props === null) {
         props = {}
